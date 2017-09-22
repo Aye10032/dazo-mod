741d1d365362b4fabc676165429950ed4a2636e7
Aye10032 <yeyu6_505@126.com>
2017-09-22 22:10:12 +0800

    update

--- src/main/java/aye10032/dazo/Dazo.java
+++ src/main/java/aye10032/dazo/Dazo.java
@@ -1,6 +1,16 @@
 package aye10032.dazo;
 
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Dazo.MODID, name=Dazo.NAME, version=Dazo.VERSION)
 public class Dazo 
 {
+	
+	public static final String MODID = "dazo";
+	public static final String NAME = "Dazo";
+	public static final String VERSION = "1.0";
+	
+	
+	
 	
 }
