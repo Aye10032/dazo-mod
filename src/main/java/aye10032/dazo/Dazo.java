18289103649f2d82fe7ac566d4baca8d2586df22
Aye10032 <yeyu6_505@126.com>
2017-09-22 22:27:10 +0800

    m

--- src/main/java/aye10032/dazo/Dazo.java
+++ src/main/java/aye10032/dazo/Dazo.java
@@ -1,6 +1,9 @@
 package aye10032.dazo;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Dazo.MODID, name=Dazo.NAME, version=Dazo.VERSION)
 public class Dazo 
@@ -11,6 +14,17 @@ public class Dazo
 	public static final String VERSION = "1.0";
 	
 	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
 	
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
 	
 }
